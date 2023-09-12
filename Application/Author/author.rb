2290eb4cbe1d7742e6b3319d177234c4ac73b495
Md. Porag Sarkar <porag-m06>
2023-09-12 15:59:11 +0600

    author update and genre class with properties, initialize, and add_item methodh implemented

--- Application/Author/author.rb
+++ Application/Author/author.rb
@@ -10,7 +10,7 @@ class Author
   end
 
   def add_item(item)
-    @items << item
     item.author = self
+    @items << item
 end
 
--- Application/Genre/genre.rb
+++ Application/Genre/genre.rb
@@ -1,2 +1,14 @@
 class Genre
+  attr_accessor :name
+  attr_reader :id, :items
+
+  def initialize(name)
+    id = Random.rand(2..2000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    item.genre = self
+    @items << item
 end
