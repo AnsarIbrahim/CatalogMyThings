bfd0e9075c9eaea5b26383f8ffe962f12ac9a7ef
Md. Porag Sarkar <porag-m06>
2023-09-12 16:03:47 +0600

    label.rb implemented with all the properties and method

--- Application/Label/label.rb
+++ Application/Label/label.rb
@@ -1,2 +1,17 @@
 class Label
+  attr_accessor :title, :color
+  attr_reader :id, :items
+
+  def initialize(title, color)
+    @id = Random.rand(3..3000)
+    @title = title
+    @color = color
+    @items = []
+  end
+
+  def add_item(item)
+    item.label = self
+    @item << item
+  end
+  
 end
