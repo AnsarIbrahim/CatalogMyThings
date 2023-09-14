faf07de555d0db276f3be0c1336dab5306a431ca
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-09-14 18:17:55 +0530

    Fix menu on App and clear unwanted list in app

--- Application/Models/menu.rb
+++ Application/Models/menu.rb
@@ -8,17 +8,17 @@ game_author = GameAuthor.new
 def main_menu(media_library, game_author)
   loop do
     display_menu
-    print 'Please select an option (1-13 or 14 to quit): '
+    print 'Please select an option (1-9 or 0 to quit): '
     choice = gets.chomp.to_i
 
     case choice
-    when 1..5
+    when 1..4
       handle_listing_option(choice, media_library, game_author)
-    when 6..8
+    when 5..6
       handle_listing_labels_or_authors(choice, media_library, game_author)
-    when 9..12
+    when 7..9
       handle_adding_option(choice, media_library, game_author)
-    when 13
+    when 0
       exit_app
       break
     else
@@ -34,34 +34,28 @@ def handle_listing_option(choice, media_library, game_author)
   when 2
     media_library.list_music_albums
   when 3
-    list_movies(media_library.movies)
-  when 4
     game_author.list_games
-  when 5
+  when 4
     media_library.list_genres
   end
 end
 
 def handle_listing_labels_or_authors(choice, media_library, game_author)
   case choice
-  when 6
+  when 5
     list_labels(media_library.labels)
-  when 7
+  when 6
     game_author.list_authors
-  when 8
-    list_sources(media_library.sources)
   end
 end
 
 def handle_adding_option(choice, media_library, game_author)
   case choice
-  when 9
+  when 7
     add_book(media_library.books)
-  when 10
+  when 8
     media_library.add_music_album
-  when 11
-    add_movie(media_library.movies)
-  when 12
+  when 9
     game_author.add_game
   end
 end
@@ -71,7 +65,7 @@ def exit_app
 end
 
 def invalid_option_message
-  puts 'Invalid option. Please select a valid option (1-13 or 14 to quit).'
+  puts 'Invalid option. Please select a valid option (1-9 or 0 to quit).'
 end
 
 main_menu(media_library, game_author)
